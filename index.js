f01f200b8747d2da0b8e51471b4fb34f1d149825
Nathan Agrin <n8agrin@gmail.com>
2015-03-17 09:04:46 -0700

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+var slm = require('slm').render;
+
+function slmPreprocessor(config, logger) {
+  var log, options;
+
+  log = logger.create('preprocessor.slm');
+  options = options || {};
+
+  return function(content, file, done) {
+    var result;
+    result = slm(content, options);
+    log('slm result', result);
+    done(null, result);
+  }
+}
+
+slmPreprocessor.$inject = ['config.slm', 'logger'];
+module.exports = {
+  'preprocessor:slm': slmPreprocessor
+}
