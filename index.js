a45884fc201c5748048c32d124e772f8afbee848
Nathan Agrin <n8agrin@gmail.com>
2015-03-17 10:07:21 -0700

    Updates to debugging and parsing with options

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 var slm = require('slm').render;
 
-function slmPreprocessor(config, logger) {
-  var log, options;
+function slmPreprocessor(config, logger, helper) {
+  var log, locals, opts;
 
   log = logger.create('preprocessor.slm');
-  options = options || {};
+  config = config || {};
+  locals = helper._.clone(config.locals);
+  options = helper._.clone(config.options);
 
   return function(content, file, done) {
     var result;
-    result = slm(content, options);
+    log.debug('slm locals', locals);
+    log.debug('slm options', options);
+    log.debug('slm content', content);
+    result = slm(content, locals, options);
     log.debug('slm result', result);
     done(null, result);
   }
 }
 
-slmPreprocessor.$inject = ['config.slm', 'logger'];
+slmPreprocessor.$inject = ['config.slm', 'logger', 'helper'];
+
 module.exports = {
   'preprocessor:slm': ['factory', slmPreprocessor]
 }
